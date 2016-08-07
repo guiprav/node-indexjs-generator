3284e37f0f8de1abd55f8670e4b4500e3577e88e
Guilherme Vieira <super.driver.512@gmail.com>
2016-08-07 04:12:35 -0300

    Initial implementation.

---
+++ index.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const glob = require('glob');
+
+const target = process.argv[2] || '.';
+
+glob.sync(`${target}/**/index.js`).forEach(indexerPath => {
+  const relIndexerPath = path.relative(target, indexerPath);
+
+  const dirPath = path.dirname(relIndexerPath);
+
+  const indexPath = `${dirPath}/index.js`;
+
+  if (!shouldIndex(indexPath)) {
+    return;
+  }
+
+  const root = {};
+
+  // FIXME: Don't ignore nested index modules.
+  glob.sync(`${dirPath}/**/!(index).js`).forEach(modPath => {
+    const relModPath = path.relative(dirPath, modPath);
+
+    const objPath = relModPath.split(path.sep);
+    let objName = path.basename(objPath.pop(), '.js');
+
+    {
+      const objDotPath = objName.split('.');
+
+      if (objDotPath.length > 1) {
+        objName = objDotPath.pop();
+        objPath.push(...objDotPath);
+      }
+    }
+
+    objPath.reduce((node, key) => {
+      let child = node[key];
+
+      if (!child) {
+        child = node[key] = {};
+      }
+
+      return child;
+    }, root)[objName] = `require('./${relModPath}')`;
+  });
+
+  const indexedModPath = `${dirPath}/.indexed.js`;
+
+  fs.writeFileSync(indexedModPath, [
+    'module.exports = ' + JSON.stringify(root, null, 2).replace(
+      /"(require\(.+\))"/g, '$1'
+    ) + ';',
+  ].join('\n'));
+});
+
+function shouldIndex(_path) {
+  let data;
+
+  try {
+    data = fs.readFileSync(_path, { encoding: 'utf8' });
+  }
+  catch(err) {
+    return false;
+  }
+
+  // FIXME: This is susceptible to false positives and negatives.
+  return data.includes(`module.exports = require('./.indexed')`);
+}
